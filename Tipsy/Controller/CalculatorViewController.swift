365338251a388834ffe8e10f8c76d69c2704d6e9
John Santiago <johnsantiago@Johns-Mac-mini.COMFAST>
2022-02-04 20:32:10 -0700

    Outlets and Actions added with selection implemented

---
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var ZeroPctLabel: UIButton!
+    @IBOutlet weak var tenPctLabel: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    
+    @IBOutlet weak var splitNumberlabel: UILabel!
+        
+   
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        if (sender.currentTitle == ZeroPctLabel.currentTitle) {
+            ZeroPctLabel.isSelected = true
+            tenPctLabel.isSelected = false
+            twentyPctButton.isSelected = false
+        } else if (sender.currentTitle == tenPctLabel.currentTitle) {
+            ZeroPctLabel.isSelected = false
+            tenPctLabel.isSelected = true
+            twentyPctButton.isSelected = false
+        } else {
+            ZeroPctLabel.isSelected = false
+            tenPctLabel.isSelected = false
+            twentyPctButton.isSelected = true
+        }
+        
+    }
+    
+    @IBAction func stepValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        if (ZeroPctLabel.isSelected) {
+            print(0.0)
+        } else if (tenPctLabel.isSelected) {
+            print(0.1)
+        } else {
+            print(0.2)
+        }
+    }
+
+}
+
---
+++ Tipsy/Controller/ResultsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by John Santiago on 2/4/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
